7f4a38b262d1047b2de61a20b6b99b2d1ae53639
jialincheoh <jcheoh@purdue.edu>
2024-04-22 18:07:00 -0400

    remove unnecessary files

--- project-3/cocktails/experiment/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    #path('', views.home, name = 'task-home'),
-    #path('dashboard/', views.dashboard, name = 'task-dashboard'),
-    #path('task1/', views.task1, name = 'task-1'),
-]
